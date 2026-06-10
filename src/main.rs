f5e370a363ed705343748a1f31a486b0e61466e3
filianore <>
2026-06-10 20:32:19 +0200

    add exitcode support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, process::ExitCode};
 
 fn create_html_link(link: &str) {
     let pre = "<html>\n<head>\n";
@@ -9,12 +9,14 @@ fn create_html_link(link: &str) {
     );
 }
 
-fn main() {
+fn main() -> ExitCode {
     // dump cli arguments to args variable
     let args = env::args().skip(1).collect::<Vec<String>>().join(" ");
     if args.len() != 0 {
         create_html_link(&args);
+        ExitCode::from(0)
     } else {
-        eprintln!("You need to provide a URL via command-line argument.")
+        eprintln!("You need to provide a URL via command-line argument.");
+        ExitCode::from(1)
     }
 }
