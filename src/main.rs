44dc8cadd06e997bb4e2bc1221e3adae46a25a7a
filianore <>
2026-06-12 18:22:28 +0200

    I like it more when it says return

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,9 @@ fn main() -> ExitCode {
     let args = args().skip(1).collect::<Vec<String>>().join(" ");
     if args.len() != 0 {
         create_html_link(&args);
-        ExitCode::from(0)
+        return ExitCode::from(0);
     } else {
         eprintln!("You need to provide a URL via command-line argument.");
-        ExitCode::from(1)
+        return ExitCode::from(1);
     }
 }
